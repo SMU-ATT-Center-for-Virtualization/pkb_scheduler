d8f1ccdfa898f691e8dc7859972362fc36dc4b52
TroyRWalker <trwalker@smu.edu>
2020-08-31 21:08:51 -0500

    Update cloud_util.py

--- cloud_util.py
+++ cloud_util.py
@@ -7,6 +7,7 @@ import re
 
 
 def cpu_count_from_machine_type(cloud, machine_type):
+  print("Cloud is set to: {}".format(cloud))
   if cloud == 'GCP':
     return int(machine_type.split('-')[2])
   elif cloud == 'AWS':

b22d30fad0f941d4ad00ecf28dcbce4d2f20b1cc
TroyRWalker <trwalker@smu.edu>
2020-10-09 02:05:04 -0500

    Update pkb_scheduler.py

--- pkb_scheduler.py
+++ pkb_scheduler.py
@@ -402,6 +402,7 @@ def create_graph_from_config_list(benchmark_config_list, pkb_command):
   
  # region_dict = cloud_util.get_region_info(benchmark_config_list[0][1]['flags']['cloud'].upper())
   full_graph.add_region_if_not_exists(new_region=benchmark_config_list[0][1]['flags']['zones'])
+  print(f"FLAGS is {FLAGS}")
   region_dict = cloud_util.get_region_info(benchmark_config_list[0][1]['flags'])
   for key in region_dict:
     # if region['description'] in full_graph.regions

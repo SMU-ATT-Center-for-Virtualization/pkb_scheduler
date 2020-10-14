74e9b7fc55262fc1d2cc579f74f61be37bce9aa1
TroyRWalker <trwalker@smu.edu>
2020-10-14 02:04:07 -0500

    Update benchmark_graph.py

--- benchmark_graph.py
+++ benchmark_graph.py
@@ -380,7 +380,7 @@ class BenchmarkGraph():
         print("here2")
         print(f"Self: {self.__dict__} \n Type: {self}")
         print(f"self.regions is: {self.regions} : the type of (regions) is: {type(self.regions)}")
-        
+        print(f"vm region is: {vm_region}")
         status = self.regions[vm_region].add_virtual_machine_if_possible(vm)
         print("Status ", status)
 

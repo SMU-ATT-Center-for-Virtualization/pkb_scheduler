64642a8eee44e756f3d0a65efce68dcdc2e8110f
TroyRWalker <trwalker@smu.edu>
2020-10-25 04:21:54 -0500

    Update benchmark_graph.py

--- benchmark_graph.py
+++ benchmark_graph.py
@@ -383,6 +383,8 @@ class BenchmarkGraph():
         print(f"\n\nSelf: {self.__dict__} \n Type: {self}\n\n")
         print(f"\n\nvm region is: {vm_region}\n\n")
         print(f"\n\nself.regions is: {self.regions} : the type of (regions) is: {type(self.regions)}\n\n")
+        myRegion = Region()
+        myRegion.name = vm_region
         self.add_region_if_not_exists(vm_region)
         status = self.regions[vm_region].add_virtual_machine_if_possible(vm)
         print("Status ", status)

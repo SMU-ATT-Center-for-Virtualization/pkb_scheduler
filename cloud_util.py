bdae37810c097524b5bd9266bdc21e1c51412213
TroyRWalker <35703217+TroyRWalker@users.noreply.github.com>
2020-12-04 01:55:53 -0800

    Update cloud_util.py

--- cloud_util.py
+++ cloud_util.py
@@ -65,7 +65,7 @@ def get_region_info(cloud):
                                stdout=subprocess.PIPE)
     output, error = process.communicate()
     # load json and convert to a more useable output
-    print(f"\n\nget region info OUTPUT of type {type(output)} IS: {output}\n\n")
+    print(f"\n\nget region info OUTPUT of type {type(output)} IS: {json.loads(output.decode('utf-8'))}\n\n")
 
     region_json = json.loads(output.decode('utf-8'))
     print(f"region_json is: {region_json}")

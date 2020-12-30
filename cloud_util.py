158ea33c69982b11cc736c660e63f7701e1693af
TroyRWalker <trwalker@smu.edu>
2020-12-30 11:25:24 -0800

    Update cloud_util.py

--- cloud_util.py
+++ cloud_util.py
@@ -76,7 +76,7 @@ def get_region_info(cloud):
     region_json = json.loads(output.decode('utf-8'))
     print(f"The amount of running's in region_list_command,[{type(region_json)}]: ")
 
-    print(f"region_json is: {region_json}")
+    #print(f"region_json is: {region_json}")
     for region_iter in region_json['Regions']:
       region_dict[region_iter['RegionName']] = {}
   else:

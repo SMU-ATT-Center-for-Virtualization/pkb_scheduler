1ef76471e90ad76592c710cab983be43302eb56c
TroyRWalker <trwalker@smu.edu>
2020-12-30 11:16:46 -0800

    Update cloud_util.py

--- cloud_util.py
+++ cloud_util.py
@@ -63,7 +63,7 @@ def get_region_info(cloud):
     #region_list_command = "date"
     #region_list_command ="aws ec2 describe-instances"
     #process = subprocess.Popen(region_list_command, stdout=subprocess.PIPE)
-    process = subprocess.Popen(region_list_command, stdout=subprocess.PIPE)
+    process = subprocess.Popen(region_list_command, stdout=subprocess.PIPE, shell=True)
 
     #This kinda works? vvvvvvvvvvvvv
     #process = subprocess.check_output(region_list_command,stderr=subprocess.STDOUT,shell=True)

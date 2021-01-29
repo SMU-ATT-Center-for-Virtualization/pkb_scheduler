3652e6ccceb749c7e773c8b738b12b93e1f3db9c
TroyRWalker <trwalker@smu.edu>
2021-01-29 14:26:46 -0600

    Update pkb_scheduler.py

--- pkb_scheduler.py
+++ pkb_scheduler.py
@@ -449,7 +449,7 @@ def create_graph_from_config_list(benchmark_config_list, pkb_command):
       benchmark_counter += 1
       logger.debug("Number of benchmarks: " + str(len(temp_benchmarks)))
       print(f"Number of benchmarks: {str(len(temp_benchmarks))}")
-      print(f"the temp benchmarks are: {temp_benchmarks.__dict__}")
+      #print(f"the temp benchmarks are: {temp_benchmarks.__dict__}")
       print(f"temp_benchmarks is {temp_benchmarks[0].__dict__}\n")
       for bm in temp_benchmarks:
         logger.debug("Trying to add " + bm.vm_specs[0].zone + " and " + bm.vm_specs[1].zone)

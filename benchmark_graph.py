660f4c61e745782a1f747b29cb9c3643f7ef5ede
TroyRWalker <trwalker@smu.edu>
2021-03-12 03:05:21 -0600

    Update benchmark_graph.py

--- benchmark_graph.py
+++ benchmark_graph.py
@@ -288,11 +288,12 @@ class BenchmarkGraph():
         return False, None
 
   def add_or_waitlist_benchmark_and_vms(self, bm, region_dict=0):
+    aws_quota_tracker = 0
     print(f"\n\naws_quota_tracker in add_or_waitlist_benchmark_and_vms: {aws_quota_tracker}\n\n")
     print(f"bm is {bm.__dict__}")
     print(f"\n\n\nDO WE EVER GET HERE benchmark_graph:add_or_waitlist_benchmark_and_vms:292\n\n\n")
     print(f"Here7\n")
-    aws_quota_tracker = 0
+   
     vms = self.add_vms_for_benchmark_if_possible(bm, region_dict, aws_quota_tracker)
     
     print(f"\n\nAdded the vms to benchmark: {vms}\n\n")

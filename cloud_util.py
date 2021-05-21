fd76d825babe9df6f4cb3c2b3d005026feb57bd3
TroyRWalker <35703217+TroyRWalker@users.noreply.github.com>
2021-05-21 00:05:44 -0700

    Update cloud_util.py

--- cloud_util.py
+++ cloud_util.py
@@ -177,7 +177,7 @@ def get_region_info(cloud):
           #print(f" variable is  and the result is: {quota_iter['currentValue']} and  {quota_iter['limit']}")
           quotaName = quota_iter["localName"]
           #print(f"region_dict is : {region_dict} \n\n quota name is: {quotaName}")
-          region_dict[region_name][quotaName.upper()] = (int(quota_iter["currentValue"]), int(quota_iter["limit"]))
+          region_dict[region_name][quotaName.upper()] = [int(quota_iter["currentValue"]), int(quota_iter["limit"])]
         #az network list-usages --location eastus --out table
         region_list_command = f'az network list-usages --location "{region_name}"'
         #print(f"region list commmand is: {region_list_command}")
@@ -192,7 +192,7 @@ def get_region_info(cloud):
           #print(f" variable is  and the result is: {quota_iter['currentValue']} and  {quota_iter['limit']}")
           quotaName = quota_iter["localName"]
           #print(f"region_dict is : {region_dict} \n\n quota name is: {quotaName}")
-          region_dict[region_name][quotaName.upper()] = (int(quota_iter["currentValue"]), int(quota_iter["limit"]))
+          region_dict[region_name][quotaName.upper()] = [int(quota_iter["currentValue"]), int(quota_iter["limit"])]
       except:
         print(f"Error occurred when reading in quotas. Region was {region_name}")
     print(f"Region Dict: {region_dict}")
